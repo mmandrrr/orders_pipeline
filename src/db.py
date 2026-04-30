3c404eedaec3ee0de035d63c4bff2ea0b0e8f741
Max <mmandrrr@gmail.com>
2026-04-30 11:48:24 +0300

    added requirements and dockerfile

--- src/db.py
+++ src/db.py
@@ -9,6 +9,6 @@ db_password = os.getenv("DATABASE_PASSWORD")
 def get_engine():
     """Creates and returns SQLAlchemy engine for PostgreSQL."""
     engine = create_engine(
-        f"postgresql://postgres:{db_password}@localhost:5432/music_db"
+        f"postgresql://postgres:{db_password}@host.docker.internal:5432/music_db"
     )
     return engine
